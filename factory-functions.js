c9d11281f7cb7a9e5b7149d1e22e063d8e80b102
roberromero <robertoromerodeveloper@gmail.com>
2022-03-15 12:51:09 +0000

    PROPERTY VALUE SHORTHAND and Destructured Assignment

--- factory-functions.js
+++ factory-functions.js
@@ -11,3 +11,25 @@ const robotFactory= (model, mobile) => {
   const tinCan = robotFactory('P-500', true);
   
   tinCan.beep();
+
+  //PROPERTY VALUE SHORTHAND
+
+  /*
+  const robotFactory= (model, mobile) => {
+    return{
+      model,
+      mobile,
+      beep(){
+        console.log('Beep Boop');
+      }
+    };
+  }
+  */
+
+  //Destructured Assignment
+  /*
+  const residence = vampire.residence; 
+  console.log(residence); // Prints 'Transylvania' 
+
+  const { residence } = vampire; 
+  console.log(residence); // Prints 'Transylvania'*/
